f59bed1ac669ce9071b4954d3411df35424e7526
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-07-10 21:28:03 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Siva's Blog
+
+To get it running:
+
+```sh
+deno task dev
+```
+
+To deploy it to Deno Deploy - clone it in a github repo and create and link a
+new project on https://dash.deno.com.
+
+Built using [deno_blog](https://github.com/denoland/deno_blog).
---
+++ main.js
@@ -0,0 +1,18 @@
+import blog from "https://deno.land/x/blog@0.3.3/blog.tsx";
+
+blog({
+  title: "Siva's Blog.",
+  author: "Siva",
+  avatar: "./siva.jpg",
+  avatarClass: "rounded-full",
+  links: [
+    { title: "Portfolio", url: "https://sivarn.com" },
+    { title: "GitHub", url: "https://github.com/sivayogasubramanian" },
+    {
+      title: "LinkedIn",
+      url: "https://www.linkedin.com/in/sivayogasubramanian",
+    },
+    { title: "Email", url: "mailto:sivayogasubramanian@gmail.com" },
+  ],
+  background: "#f7fbff",
+});
