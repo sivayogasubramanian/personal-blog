6800e84980f5bedb26e118c489f1840b3bfd4e99
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-07-13 10:25:38 +0800

    Change some styling

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 import blog from "https://deno.land/x/blog@0.4.1/blog.tsx";
-import { IconPortfolio } from "./components.tsx";
+import {
+  IconPortfolio,
+  IconGithub,
+  IconLinkedin,
+  IconEmail,
+} from "./components.tsx";
 
 blog({
   title: "Siva's Blog.",
@@ -8,6 +13,7 @@ blog({
   avatar: "./siva.jpg",
   avatarClass: "rounded-full",
   cover: "./cover.jpg",
+  coverTextColor: "#f7fbff",
   links: [
     {
       title: "Portfolio",
@@ -17,17 +23,19 @@ blog({
     {
       title: "GitHub",
       url: "https://github.com/sivayogasubramanian",
+      icon: IconGithub(),
     },
     {
       title: "LinkedIn",
       url: "https://www.linkedin.com/in/sivayogasubramanian",
+      icon: IconLinkedin(),
     },
     {
       title: "Email",
       url: "mailto:sivayogasubramanian@gmail.com",
+      icon: IconEmail(),
     },
   ],
-  theme: "dark",
   favicon: "./favicon.ico",
   ogImage: "./og.jpg",
   dateStyle: "long",
